608e0d00f7c000df8250f47dd79a63b59adfd3e2
Yunsung Choi <36170628+YunsungChoi@users.noreply.github.com>
2019-06-10 22:00:07 -0700

    Assignment9 isSorted()
    LinkedIntList and ListNode files.

---
+++ LinkedIntList.java
@@ -0,0 +1,140 @@
+// Class LinkedIntList can be used to store a list of integers.
+
+public class LinkedIntList {
+    private ListNode front;  // first value in the list
+
+    // post: constructs an empty list
+    public LinkedIntList() {
+        front = null;
+    }
+
+    // post: returns the current number of elements in the list
+    public int size() {
+        int count = 0;
+        ListNode current = front;
+        while (current != null) {
+            current = current.next;
+            count++;
+        }
+        return count;
+    }
+
+    // pre : 0 <= index < size()
+    // post: returns the integer at the given index in the list
+    public int get(int index) {
+        return nodeAt(index).data;
+    }
+
+    // post: creates a comma-separated, bracketed version of the list
+    public String toString() {
+        if (front == null) {
+            return "[]";
+        } else {
+            String result = "[" + front.data;
+            ListNode current = front.next;
+            while (current != null) {
+                result += ", " + current.data;
+                current = current.next;
+            }
+            result += "]";
+            return result;
+        }
+    }
+
+    // post : returns the position of the first occurrence of the given
+    //        value (-1 if not found)
+    public int indexOf(int value) {
+        int index = 0;
+        ListNode current = front;
+        while (current !=  null) {
+            if (current.data == value) {
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    // post: appends the given value to the end of the list
+    public void add(int value) {
+        if (front == null) {
+            front = new ListNode(value);
+        } else {
+            ListNode current = front;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new ListNode(value);
+        }
+    }
+
+    // pre: 0 <= index <= size()
+    // post: inserts the given value at the given index
+    public void add(int index, int value) {
+        if (index == 0) {
+            front = new ListNode(value, front);
+        } else {
+            ListNode current = nodeAt(index - 1);
+            current.next = new ListNode(value, current.next);
+        }
+    }
+
+    // pre : 0 <= index < size()
+    // post: removes value at the given index
+    public void remove(int index) {
+        if (index == 0) {
+            front = front.next;
+        } else {
+            ListNode current = nodeAt(index - 1);
+            current.next = current.next.next;
+        }
+    }
+
+    // pre : 0 <= i < size()
+    // post: returns a reference to the node at the given index
+    private ListNode nodeAt(int index) {
+        ListNode current = front;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+    
+    //Assignment9 ---------------------------------------------
+    public boolean isSorted() {
+    	
+    	//if empty, return true
+    	if(front == null) return true;
+    	
+    	//first value in the list = current
+    	ListNode cur = front;
+    	
+    	//while it has next value
+    	while(cur.next !=null) {
+    		
+    		// if the current value is already greater than the value of the next,
+    		// it's not sorted therefore return false
+    		if(cur.data > cur.next.data) {
+    			return false;
+    			
+    			//if not, put next value in current and check for the next value
+    		} cur = cur.next;
+    	}
+    	return true;
+    }
+    
+    public static void main(String[] args) {
+    	LinkedIntList lil = new LinkedIntList();
+    	lil.add(1);
+    	lil.add(2);
+    	lil.add(3);
+    	lil.add(9);
+    	lil.add(5);
+    	lil.add(6);
+    	lil.add(7);
+    	
+    	System.out.println(lil.isSorted()); // must return false.
+    	
+    }
+}
